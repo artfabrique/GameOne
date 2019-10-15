80ed85ddf40d13d093d4deaf63d853dc9e9853df
Alexander Vinogradov <artfabrique@gmail.com>
2019-10-15 12:53:41 +0300

    -Added DOTween -Created basic show/hide bolt macro

---
+++ Assets/Scripts/TweenOptionsHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using DG.Tweening.Core;
+using System.Reflection;
+using System;
+
+namespace Startle.Helpers
+{
+    public class TweenOptionsHelper
+    {
+
+        public void SetTweenOptions(Tween tween, float delay, Ease easing, LoopType loopType, int loops, out Tween tweenOut)
+        {
+            tweenOut = tween;
+            tweenOut.SetEase(easing).SetLoops(loops, loopType).SetDelay(delay);
+        }
+        public void SetCurveEase(Tween tween, AnimationCurve curve, out Tween tweenOut, out TweenParams tweenParams)
+        {
+            tweenOut = tween;
+            tweenParams = new TweenParams().SetEase(curve);
+            tweenOut.SetAs(tweenParams);
+        }
+        public void SetAutoKill(Tween tween, bool autoKill, out Tween tweenOut)
+        {
+            tweenOut = tween;
+            tweenOut.SetAutoKill(autoKill);
+        }
+    }
+}
